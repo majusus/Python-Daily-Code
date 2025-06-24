4963b05b4910c499e0b265c0b8b40c253d3a4e1d
majusus <susham.1338@outlook.com>
2025-06-24 16:45:03 +0530

    Day 6 added

---
+++ Day 6/Day-6_task.py
@@ -0,0 +1,25 @@
+#Task 1 - Functions
+def my_function():
+    print("Hello, World!")
+my_function()
+
+
+
+#Task 2 - Maze Project Reeborg
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
